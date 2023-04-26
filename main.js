2f7893a3d282079383bce7dc6b1c06d71fa466de
Bogdana111 <124085296+Bogdana111@users.noreply.github.com>
2023-04-26 02:07:54 +0200

    add js

---
+++ main.js
@@ -0,0 +1,20 @@
+// const fruits = ["Яблуко", "Груша", "Апельсин"];
+// const shoppingCart = fruits;
+// shoppingCart.push("Банан");
+// console.log(fruits.length);
+
+// function multiply(x, y, z) {
+//   console.log(`Результат множення дорівнює ${x * y * z}`);
+// }
+// // 2. Передача аргументів
+// multiply(2, 10, 5);
+
+// function multiply(x, y, z) {
+//   console.log("Код до return виконується звичайним чином");
+//   // Повертаємо результат виразу множення
+//   return x * y * z;
+//   console.log("Цей лог ніколи не виконається, він стоїть після return");
+// }
+// // Результат роботи функції можна зберегти у змінну
+// let result = multiply(2, 3, 5);
+// console.log(result);
