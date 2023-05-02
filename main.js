922e00368bb1ea2b7191520b9234a13231c1ca82
Bogdana111 <124085296+Bogdana111@users.noreply.github.com>
2023-05-02 02:10:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,3 +18,62 @@
 // // Результат роботи функції можна зберегти у змінну
 // let result = multiply(2, 3, 5);
 // console.log(result);
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Change code below this line
+//   const words = message.split(" ");
+//   const messageLength = words.length;
+//   return messageLength * pricePerWord;
+
+//   // Change code above this line
+// }
+
+// const planets = ["Earth", "Mars", "Venus"];
+
+// for (let i = 0; i < planets.length; i += 1) {
+//   console.log(planets[i]);
+// }
+
+// function getEvenNumbers(start, end) {
+//   // Change code below this line
+//   let evenNumbers = [];
+//   for (i = start; i <= end; i += 1) {
+//     if (i % 2 === 0) {
+//       evenNumbers.push(i);
+//     }
+//   }
+
+//   return evenNumbers;
+// }
+// console.log(getEvenNumbers(2, 5));
+
+// for (let i = 0; i <= 5; i += 1) {
+//   console.log(i);
+
+//   if (i === 3) {
+//     console.log("Знайшли число 3, перериваємо виконання циклу");
+//     break;
+//   }
+// }
+
+// console.log("Лог після циклу");
+
+// function fn() {
+//   for (let i = 0; i <= 5; i += 1) {
+//     console.log(i);
+
+//     if (i === 3) {
+//       console.log(
+//         "Знайшли число 3, робимо повернення, перериваючи цикл і функцію"
+//       );
+//       return i;
+//     }
+//   }
+
+//   // Цей console.log не виконається
+//   console.log("Лог після циклу в тілі функції");
+// }
+
+// const result = fn();
+// console.log("Лог після виходу з функції");
+// console.log(`Результат виконання функції ${result}`);
