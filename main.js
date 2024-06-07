b66928364c1c90bdcffe2500cfff075c2f19fd00
Bohdana <LipskaBogdana1990@gmail.com>
2024-06-07 03:38:34 +0200

    add changes

--- main.js
+++ main.js
@@ -516,11 +516,35 @@
 // };
 
 // **************************************************
-function getExtremeScores(scores) {
-  let obj = {};
+// function getExtremeScores(scores) {
+//   let obj = {};
 
-  obj.best = Math.max(...scores);
-  obj.worst = Math.min(...scores);
-  return obj;
-}
-console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
+//   obj.best = Math.max(...scores);
+//   obj.worst = Math.min(...scores);
+//   return obj;
+// }
+// console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
+
+// *************************************
+
+// const profile = {
+//   username: "Jacob",
+//   playTime: 300,
+//   changeUsername(newName) {
+//     this.username = newName;
+//   },
+//   updatePlayTime(hours) {
+//     this.playTime += hours;
+//   },
+//   getInfo() {
+//     return `${this.username} has ${this.playTime} active hours!`;
+//   },
+// };
+
+// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+// profile.changeUsername("Marco");
+// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+// profile.updatePlayTime(20);
+// console.log(profile.getInfo()); // "Marco has 320 active hours!"
